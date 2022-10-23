72727eeae9c2ba766312d2ce2bdda0a9b3a1c21b
vijaymane042 <vijaymane042@gmail.com>
2022-10-23 12:54:58 +0530

    updated as per the code review

--- src/main/java/com/qa/opencart/pages/SubmitPaymentTest.java
+++ src/main/java/com/qa/opencart/pages/SubmitPaymentTest.java
@@ -1,9 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class SubmitPaymentTest {
 
+	WebDriver driver;
+	private By text = By.id("login");
 
 	public void sum() {
 		System.out.println("Sum");
+		driver.findElement(text).click();
 	}
 }
