b1981de64f8505b6ffbc584c881d6f7026a82104
vijaymane042 <vijaymane042@gmail.com>
2022-10-25 17:43:39 +0530

    resolved conflict kept my code

--- src/test/java/com/qa/opencart/testcases/TestStash.java
+++ src/test/java/com/qa/opencart/testcases/TestStash.java
@@ -3,7 +3,9 @@ package com.qa.opencart.testcases;
 public class TestStash {
 	
 	public void stashTest() {
-		System.out.println("Written by vijay---Remote");
+
+		System.out.println("Written by vijay---local");
+
 	}
 
 }
